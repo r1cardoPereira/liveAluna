1393f69762726e468347b13e3ca4c8b197a2cee5
Ricardo Pereira <103537193+r1cardoPereira@users.noreply.github.com>
2022-07-15 23:45:57 -0300

    Consumindo API AMDb

--- src/App.java
+++ src/App.java
@@ -1,5 +1,18 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        var url = "https://imdb-api.com/en/API/Top250Movies/k_pk7iur0f";
+        URI uri = URI.create(url);
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder(uri).build();
+        String json = client.send(request, HttpResponse.BodyHandlers.ofString()).body();
+
+        System.out.println(json);
+
     }
 }
